f9ca0c302f7ac5b0f5f5c4b794c3579499349f98
Heinrich <freelanceharms@gmail.com>
2021-04-05 14:43:44 -0500

    added command line management interface

--- management.py
+++ management.py
@@ -6,7 +6,7 @@ def get_macros():
     """
     Get macros from backend database.
     """
-    return json.loads(requests.get("http://127.0.0.1:5000/macros"))
+    return json.loads(requests.get("http://127.0.0.1:5000/macros").content)["macros"]
 
 
 def new_macro(name: str, cmd: str):
@@ -31,3 +31,31 @@ def set_macro_attribute(id: int, attribute: str, value: str):
     requests.post(f"http://127.0.0.1:5000/macros/{id}",
                   json={"attribute": attribute, "value": value})
 
+
+if __name__ == "__main__":
+    print("Welcome to the Elenchos Management System!")
+    print("""Commands:
+list:\t\t\t\tprint out a list of existing macros
+new,<name>,<cmd>:\t\tcreate a new macro with a given name and command
+delete,<id>:\t\t\tdelete macro with given id
+set,<id>,<attribute>,<value>:\tset a given attribute to a given value for a given macro
+quit:\t\t\t\tExit Elenchos Management System\n""")
+
+    cmd = ""
+    while cmd != "quit":
+        cmd, *args = input("> ").split(",")
+
+        if cmd == "list":
+            macros = get_macros()
+            if macros:
+                print("name, id, priority, selected, cmd")
+                for macro in macros:
+                    print(f"{macro['name']},{macro['cmd']},{macro['priority']},{macro['selected']},{macro['cmd']}")
+            else:
+                print("No macros currently exist in database.")
+        elif cmd == "new":
+            new_macro(*args)
+        elif cmd == "delete":
+            delete_macro(*args)
+        elif cmd == "set":
+            set_macro_attribute(*args)
